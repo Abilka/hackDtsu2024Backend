20786c83930eb27d39858d8ec7ffef58204b343f
Alexey Shashkin <lexa.161.61@gmail.com>
2024-04-19 23:21:20 +0300

    я дурак

--- setting.py
+++ setting.py
@@ -1,4 +1,4 @@
 IP = '0.0.0.0'
 PORT = 1290
 
-DATABASE_CONNECT = "postgresql+psycopg2://postgres:vb12jol901@45.155.207.232:5432/rostov_citizen"
+DATABASE_CONNECT = f"postgresql+psycopg2://localhost:admin@45.155.207.232:5432/rostov_citizen"
