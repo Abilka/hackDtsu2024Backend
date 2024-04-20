bdcb91e1db32a75052e4ddccefb7153a9f9b9aaa
Alexey Shashkin <lexa.161.61@gmail.com>
2024-04-20 03:52:38 +0300

    багфиксы

--- api/user/privileges/card/sql.py
+++ api/user/privileges/card/sql.py
@@ -8,6 +8,7 @@ class Database(db.Database):
         user_id = self.get_user_id_by_card(card_hash)
         if not user_id:
             return "Нет такой карты"
+
         return self.get_privileges_by_user_id(user_id[-1])
 
     def get_user_id_by_card(self, card_hash: str):
@@ -21,3 +22,5 @@ class Database(db.Database):
         statement = sqlalchemy.select(table.Privileges.privileges_prefix).where(table.Privileges.user_id == user_id)
         result = self.connect.execute(statement).fetchall()
         return list(map(lambda x: x[-1], result))
+
+
--- api/user/privileges/set/sql.py
+++ api/user/privileges/set/sql.py
@@ -7,6 +7,10 @@ class Database(db.Database):
     def insert_privilege(self, **kwargs):
         if not self.select_prefix(kwargs['privileges_prefix']):
             return 'Неправильный префикс'
+
+        if not self.user_is_instance(kwargs['user_id']):
+            return 'Такого пользователя не существует'
+
         statement = sqlalchemy.dialects.postgresql.insert(table.Privileges).values(kwargs)
         statement = statement.on_conflict_do_nothing()
         self.connect.execute(statement)
@@ -19,4 +23,9 @@ class Database(db.Database):
         if result is None:
             return False
         return True
+
+    def user_is_instance(self, user_id: int):
+        statement = sqlalchemy.select(table.User.id).where(table.User.id == user_id)
+        result = self.connect.execute(statement).fetchone()
+        return False if result is None else True
 
