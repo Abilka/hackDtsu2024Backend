7322d0d28a90140ae31459f9c1ecc66056e0ca4d
Alexey Shashkin <lexa.161.61@gmail.com>
2024-04-20 21:26:47 +0300

    теперь метод работает

--- api/user/me/__init__.py
+++ api/user/me/__init__.py
@@ -4,14 +4,12 @@ from app import app
 
 from .sql import *
 from .obj import *
-import hashlib
+
 from fastapi import Response, status
 
-@app.get("/api/user/login")
-def auth_user(username: str, password: str):
-    password = hashlib.sha256(password.encode('utf-8')).hexdigest()
-    result = Database().new_token(username=username, password=password)
-    return Response(status_code=status.HTTP_401_UNAUTHORIZED) if not result else obj.AnswerLogin(result)
+@app.get("/api/user/me")
+def get_user_info(auth_token: str):
+    return sql.Database().get(auth_token)
 
 
 
