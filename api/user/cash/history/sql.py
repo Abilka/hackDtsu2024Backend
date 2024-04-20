2f566fc15c5337a27dad4c5c316a74d5ff0bc905
Alexey Shashkin <lexa.161.61@gmail.com>
2024-04-20 21:58:49 +0300

    добавлены категории

--- api/user/cash/history/sql.py
+++ api/user/cash/history/sql.py
@@ -7,6 +7,7 @@ class Database(db.Database):
     def get(self, token: str):
         user_id = api.user.sql.Database().get_by_token(token)
         user_id = user_id.id
-        statement = sqlalchemy.select(table.CashLog.timestamp, table.CashLog.price, table.CashLog.reason, table.CashLog.inn).where(table.CashLog.user_id == user_id)
+        statement = sqlalchemy.select(table.CashLog.timestamp, table.CashLog.price, table.CashLog.reason, table.CashLog.inn, table.CashLog.category_id).where(table.CashLog.user_id == user_id)
         result = self.connect.execute(statement).fetchall()
-        return list(map(lambda x: CashOperation(*x), result))
+        cash_list = list(map(lambda x: CashOperation(*x), result))
+        return sorted(cash_list, key=lambda x: x.date, reverse=True)
