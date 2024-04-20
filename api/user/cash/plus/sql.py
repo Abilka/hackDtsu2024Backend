fc7dbc4368409f5c70c1f51a9f083f84b4b99ad2
Alexey Shashkin <lexa.161.61@gmail.com>
2024-04-20 21:58:42 +0300

    добавлено пополнение

--- api/user/cash/plus/sql.py
+++ api/user/cash/plus/sql.py
@@ -11,7 +11,7 @@ import api.user.cash.sql
 
 class Database(db.Database):
 
-    def plus(self, data: CashOperation, **kwargs):
+    def plus(self, data: CashOperation):
         user_id = sql.Database().get_user_id_by_card(data.card_hash)
 
         user_balance = api.user.cash.sql.Database().get_balance_user(user_id)
@@ -19,12 +19,13 @@ class Database(db.Database):
         values = {}
         values.update({'user_id': user_id})
         values.update({'price': data.price})
-        values.update({'reason': data.reason})
+        values.update({'reason': "Пополнение на сумму " + str(data.price)})
         values.update({'inn': data.inn})
+        values.update({'category_id': -2})
 
         statement = sqlalchemy.insert(table.CashLog).values(values)
         self.connect.execute(statement)
-        statement = sqlalchemy.update(table.User).where(table.User.id == user_id).values(cash=user_balance - data.price)
+        statement = sqlalchemy.update(table.User).where(table.User.id == user_id).values(cash=user_balance + data.price)
         self.connect.execute(statement)
         self.connect.commit()
-        return {'result': {'balance': {'card_hash': data.card_hash, 'balance': user_balance - data.price}}}
+        return {'result': {'balance': {'card_hash': data.card_hash, 'balance': user_balance + data.price}}}
