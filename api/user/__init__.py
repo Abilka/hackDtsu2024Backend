96c59c1c0b3e69d27244535b95caca673a3c468a
Alexey Shashkin <lexa.161.61@gmail.com>
2024-04-21 10:14:10 +0300

    новая адресация метода

--- api/user/__init__.py
+++ api/user/__init__.py
@@ -1,7 +1,7 @@
 from app import app
-from api.user import register, login, card, privileges, admin, cash, me
+from api.user import register, login, card, privileges, admin, cash, me, username
 from .sql import *
-@app.get("/api/user")
+@app.get("/api/user", tags=['Пользователь'])
 def get_all_user(auth_admin):
     return Database().select_all_user(auth_admin=auth_admin)
 
