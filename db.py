36a2142bf32469ec115199a5230dd91f6793460b
Alexey Shashkin <lexa.161.61@gmail.com>
2024-04-21 10:14:29 +0300

    фикс идлов

--- db.py
+++ db.py
@@ -8,6 +8,9 @@ class Database:
         self.engine = sqlalchemy.create_engine(setting.DATABASE_CONNECT)
         self.connect = self.engine.connect()
 
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.connect.close()
+
 def create_base():
     engine = sqlalchemy.create_engine(setting.DATABASE_CONNECT)
     Base.metadata.create_all(engine)
